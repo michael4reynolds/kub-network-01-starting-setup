8f9f5b54bf5b3cf1d266aff70d955ca453aa4dab
Michael Reynolds <michael4reynolds@gmail.com>
2021-05-22 09:52:18 -0400

    Adding a Containerized Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,65 @@
+import './App.css';
+
+import React, {
+  useCallback, useEffect, useState,
+} from 'react';
+
+import NewTask from './components/NewTask';
+import TaskList from './components/TaskList';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const fetchTasks = useCallback(function () {
+    fetch('http://localhost:8000/tasks', {
+      headers: {
+        Authorization: 'Bearer abc',
+      },
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (jsonData) {
+        setTasks(jsonData.tasks);
+      });
+  }, []);
+
+  useEffect(
+    function () {
+      fetchTasks();
+    },
+    [fetchTasks]
+  );
+
+  function addTaskHandler(task) {
+    fetch('http://localhost:8000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer abc',
+      },
+      body: JSON.stringify(task),
+    })
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (resData) {
+        console.log(resData);
+      });
+  }
+
+  return (
+    <div className="App">
+      <section>
+        <NewTask onAddTask={addTaskHandler} />
+      </section>
+      <section>
+        <button onClick={fetchTasks}>Fetch Tasks</button>
+        <TaskList tasks={tasks} />
+      </section>
+    </div>
+  );
+}
+
+export default App;
